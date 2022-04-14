3f14010951565f93d2527d76872d0f56df6b6014
New Author Name <dennise.e.camacho@gmail.com>
2022-04-14 11:17:15 -0500

    junit testing practice, hash test

---
+++ src/main/java/CodeupCrypt.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+public class CodeupCrypt {
+
+    public static double version;
+   public static String hashPassword(String usersWord){
+       String hash = "";
+       for (char character : usersWord.toCharArray()){
+
+       }
+
+
+   }
+
+}
